0c9ed9bda088e8e24456eef7380f22b0e26edcc1
yangtzu0506 <yangtzu0506@gmail.com>
2022-05-06 15:49:03 +0800

    0506管理者編輯

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $labeltxt=$_GET["label"];
                   <!-- Link--><a class="nav-link active" href="index.php">拾獲物管理</a>
                 </li>
                 <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="pagesDropdown" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">尋物啟示管理</a>
-                  <div class="dropdown-menu mt-3 shadow-sm" aria-labelledby="pagesDropdown"><a class="dropdown-item border-0 transition-link" href="index.html">Homepage</a><a class="dropdown-item border-0 transition-link" href="shop.html">Category</a><a class="dropdown-item border-0 transition-link" href="detail.html">Product detail</a><a class="dropdown-item border-0 transition-link" href="cart.html">Shopping cart</a><a class="dropdown-item border-0 transition-link" href="checkout.html">Checkout</a></div>
+                  <div class="dropdown-menu mt-3 shadow-sm" aria-labelledby="pagesDropdown"><a class="dropdown-item border-0 transition-link" href="index.html"></a><a class="dropdown-item border-0 transition-link" href="shop.html">Category</a><a class="dropdown-item border-0 transition-link" href="detail.html">Product detail</a><a class="dropdown-item border-0 transition-link" href="cart.html">Shopping cart</a><a class="dropdown-item border-0 transition-link" href="checkout.html">Checkout</a></div>
                 </li>
                 <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="pagesDropdown" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">貼文審核</a>
                   <div class="dropdown-menu mt-3 shadow-sm" aria-labelledby="pagesDropdown"><a class="dropdown-item border-0 transition-link" href="index.html">Homepage</a><a class="dropdown-item border-0 transition-link" href="shop.html">Category</a><a class="dropdown-item border-0 transition-link" href="detail.html">Product detail</a><a class="dropdown-item border-0 transition-link" href="cart.html">Shopping cart</a><a class="dropdown-item border-0 transition-link" href="checkout.html">Checkout</a></div>
@@ -277,6 +277,7 @@ $labeltxt=$_GET["label"];
                   <?php
                     while($record=mysqli_fetch_row($rs))
                     {
+                      $id=$record[0];
                       $name=$record[1];
                       $text=$record[2];
                       $time=$record[3];
@@ -314,6 +315,55 @@ $labeltxt=$_GET["label"];
         </div>
       </div>
       <!-- 編輯資料彈窗 -->
+      <script type="text/javascript">
+            function label(e){
+              
+              var label=e;
+            
+            var sectors=new Array();
+            sectors[0]=['請選擇標籤'];
+            sectors[1]=['外套','上衣','褲子','襪子','鞋子'];
+            sectors[2]=['帽子','眼鏡','手錶','項鍊','戒指','耳環','眼鏡'];
+            sectors[3]=['學生證','身分證','駕照'];
+            sectors[4]=['手機','耳機','電腦','平板','滑鼠','音響','相機'];
+            sectors[5]=['肩包','腰包','手提包'];
+            sectors[6]=['錢包','雨傘','香水','打火機'];
+            sectors[7]=['鉛筆盒','文具'];
+            var index;
+            switch(label){
+              case "衣物":
+              index=1;
+              break;
+              case "配件":
+              index=2;
+              break;
+              case "證件":
+              index=3;
+              break;
+              case "3C產品":
+              index=4;
+              break;
+              case "包包":
+              index=5;
+              break;
+              case "隨身物品":
+              index=6;
+              break;
+              case "文教用品":
+              index=7;
+              break;
+            }
+    
+            var Sinner="";
+            for(var i=0;i<sectors[index].length;i++){
+              
+                Sinner=Sinner+'<option value='+sectors[index][i]+'>'+sectors[index][i]+'</option>';
+                          }
+            var sectorSelect=document.getElementById("labelSelect");
+            sectorSelect.innerHTML=Sinner;
+        
+              }
+                  </script>
       <div class="modal fade" id="<?php echo $name?>edit" tabindex="-1">
         <div class="modal-dialog modal-lg modal-dialog-centered">
           <div class="modal-content overflow-hidden border-0">
@@ -323,16 +373,37 @@ $labeltxt=$_GET["label"];
                 <div class="col-lg-6 p-lg-0"><a class="glightbox product-view d-block h-100 bg-cover bg-center" style="background: url(<?php echo $img?>)" href="<?php echo $img?>" data-gallery="gallery1" data-glightbox="<?php echo $name?>"></a></div>
                 <div class="col-lg-6">
                   <div class="p-4 my-md-4">
-                    <form method="post" action="index.php">
-                    <h4 class="h4">名稱</h4><input class="form-control form-control" type="text" name="item_name" value=<?php echo $name?>>
-                    <p class="text-muted">敘述</p><input class="form-control form-control-lg" type="text" name="item-text" value=<?php echo $text?>>
-                    <p class="text-mb mb-4">遺失時間：<?php echo $time?></p>
-                    <div class="row align-items-stretch mb-4 gx-0">
-                      <div class="col-sm-7">
-                      <p class="text-mb mb-4">遺失地點：<?php echo $place?></p>
-                      <p class="text-mb mb-4">標籤：<?php echo $label?></p>
-                      </div>
+                    <form method="post" action="edit_post.php?item_id=<?php echo $id?>">
+                    <label class="h4">名稱</label><input class="form-control" type="text" name="item_name" value=<?php echo $name?>>
+                    <label class="text-muted">敘述</label><input class="form-control form-control-lg" type="text" name="item_text" value=<?php echo $text?>>
+                    <label class="text-muted">遺失時間</label><input class="form-control" type="datetime-local" name="item_time" value=<?php echo $time?>>
+                    <div class="row align-items-stretch mb-4 gx-0"> 
+                    <label class="text-muted">遺失地點</label><input class="form-control" type="text" name="item_place" value=<?php echo $place?>>
+                    <div class="col-lg-12 btn-group-toggle" id="labelName" >
+                  <label class="form-label text-muted" for="">標籤 </label><br>
+                  <input type="radio" class="btn-check" name="btnradio" id="衣物" autocomplete="off" value="衣物" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="衣物">衣物</label>&nbsp&nbsp
+                  <input type="radio" class="btn-check" name="btnradio" id="配件" autocomplete="off" value="配件" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="配件">配件</label>&nbsp&nbsp
+                  <input type="radio" class="btn-check" name="btnradio" id="證件" autocomplete="off" value="證件" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="證件">證件</label>&nbsp&nbsp
+                  <input type="radio" class="btn-check" name="btnradio" id="3C產品" autocomplete="off" value="3C產品" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="3C產品">3C產品</label>&nbsp&nbsp
+                  <input type="radio" class="btn-check" name="btnradio" id="包包" autocomplete="off" value="包包" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="包包">包包</label>&nbsp&nbsp
+                  <input type="radio" class="btn-check" name="btnradio" id="隨身物品" autocomplete="off" value="隨身物品" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="隨身物品">隨身物品</label>&nbsp&nbsp
+                  <input type="radio" class="btn-check" name="btnradio" id="文教用品" autocomplete="ff" value="文教用品" onchange="label(this.value)">
+                  <label class="btn btn-outline-info" for="文教用品">文教用品</label>&nbsp&nbsp
+                </div>
+                
+                <div class="col-lg-12 form-group">
+                    <select class="show-tick form-control" id="labelSelect" name="item_label" data-customclass="form-control form-control-lg rounded-0" >
+                    <option disabled selected >請選擇標籤</option> 
+                  </select>
+                  </div>
                     </div>
+                  <input type="submit" class="btn btn-info" value="儲存">
                     </form>
                   </div>
                 </div>
@@ -348,7 +419,6 @@ $labeltxt=$_GET["label"];
                         <div class='badge text-white bg-primary'><?php if($confirm==0){ echo "未認證"; } ?></div><a class='d-block' href='#'><img class='img-fluid w-100' src="<?php echo $img ?>" alt='...'></a>
                         <div class='product-overlay'>
                           <ul class='mb-0 list-inline'>
-                    
                             <li class='list-inline-item m-0 p-0'><a class='btn btn-sm btn-outline-dark' href='cart.html'>認領</a></li>
                             <li class='list-inline-item m-0 p-0'><a class='btn btn-sm btn-outline-dark' href="#<?php echo $name?>" data-bs-toggle='modal'><i class='fas fa-expand'></i></a></li>
                             <?php if($_SESSION["level"]=='1'){ ?>
