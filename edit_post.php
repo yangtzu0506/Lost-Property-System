740f319d5c46fe3ec852621087791e9371209325
fach <lesly2000105@gmail.com>
2022-05-24 20:44:12 +0800

    test

---
+++ edit_post.php
@@ -0,0 +1,25 @@
+<?php
+include "connect.php";
+$item_id = $_GET["item_id"];
+$item_name = $_POST["item_name"];
+$item_text = $_POST["item_text"];
+$item_time  = $_POST["item_time"];
+$item_place = $_POST["item_place"];
+$item_label = $_POST["item_label"];
+
+//上傳圖片
+$sql="update item set item_name = '$item_name',item_text='$item_text',item_time='$item_time',item_place='$item_place',item_label='$item_label' where item_id=$item_id";
+    echo $sql;
+    if(mysqli_query($link,$sql))
+    {
+        header('location:index.php');
+    } 
+    else{?>
+    <script> 
+    alert("編輯錯誤，請檢查資料");
+    location.href="index.php";
+
+    </script>
+<?php
+        }
+?>
